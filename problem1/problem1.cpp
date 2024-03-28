82ff9bcd999f96886b8bfbdf1908e826e93143b6
Logan Feeney <lmf5732@psu.edu>
2024-03-28 09:54:01 -0400

    finishing problem 1

--- problem1/problem1.cpp
+++ problem1/problem1.cpp
@@ -1,95 +1,116 @@
 /************************************************************************************************************************
 Logan Feeney
 Homework 2
-Probem 3
+Probem 1
 3/27/24
 ************************************************************************************************************************/
+
 #include <iostream>
 #include <string>
 #include <vector>
 
-// Abstract base class for sensors
-class Sensor {
+// abstract base class called "Sensor" (question 1.1)
+class Sensor 
+{
 public:
-    virtual void gatherData() const = 0;
-    virtual void processData() const = 0;
-    virtual ~Sensor() {}
+    virtual void gatherData() const = 0;        // virtual funciton for data collection
+    virtual void processData() const = 0;       // virtual function for data processing
+    virtual ~Sensor() {}                        // virtual destructor
 };
 
-// Derived class for temperature sensor
-class TemperatureSensor : public Sensor {
+// derived class for temperature sensor (question 1.2)
+class TemperatureSensor : public Sensor 
+{
 public:
-    void gatherData() const override {
-        std::cout << "Gathering data from Temperature Sensor." << std::endl;
+    void gatherData() const override 
+    {
+        std::cout << "Gathering data from temperature sensor." << std::endl;
     }
 
-    void processData() const override {
-        std::cout << "Processing data from Temperature Sensor." << std::endl;
-    }
-};
-
-// Derived class for pressure sensor
-class PressureSensor : public Sensor {
-public:
-    void gatherData() const override {
-        std::cout << "Gathering data from Pressure Sensor." << std::endl;
-    }
-
-    void processData() const override {
-        std::cout << "Processing data from Pressure Sensor." << std::endl;
+    void processData() const override 
+    {
+        std::cout << "Processing data from temperature sensor." << std::endl;
     }
 };
 
-// Derived class for position sensor
-class PositionSensor : public Sensor {
+// derived class for heading sensor (question 1.2)
+class HeadingSensor : public Sensor 
+{
 public:
-    void gatherData() const override {
-        std::cout << "Gathering data from Position Sensor." << std::endl;
+    void gatherData() const override 
+    {
+        std::cout << "Gathering data from heading sensor." << std::endl;
     }
 
-    void processData() const override {
-        std::cout << "Processing data from Position Sensor." << std::endl;
+    void processData() const override 
+    {
+        std::cout << "Processing data from heading sensor." << std::endl;
     }
 };
 
-// Factory class to create sensors
-class SensorFactory {
+// derived class for attitude sensor (question 1.2)
+class AttitudeSensor : public Sensor 
+{
 public:
-    static Sensor* createSensor(const std::string& sensorType) {
-        if (sensorType == "Temperature") {
+    void gatherData() const override 
+    {
+        std::cout << "Gathering data from attitude sensor." << std::endl;
+    }
+
+    void processData() const override 
+    {
+        std::cout << "Processing data from attitude sensor." << std::endl;
+    }
+};
+
+// another class name SensorFactory (question 1.3)
+class SensorFactory 
+{
+public:
+    static Sensor* createSensor(const std::string& sensorType) 
+    {
+        if (sensorType == "Temperature") 
+        {
             return new TemperatureSensor();
         }
-        else if (sensorType == "Pressure") {
-            return new PressureSensor();
+        else if (sensorType == "Heading") 
+        {
+            return new HeadingSensor();
         }
-        else if (sensorType == "Position") {
-            return new PositionSensor();
+        else if (sensorType == "Attitude") 
+        {
+            return new AttitudeSensor();
         }
-        else {
-            return nullptr; // Unknown sensor type
+        else 
+        {
+            return nullptr; // return null pointer if input string does not match a sensor type
         }
     }
 };
 
-// Control system class
-class AerospaceControlSystem {
+// control system class called "AerospaceControlSystem" (question 1.4)
+class AerospaceControlSystem 
+{
 private:
     std::vector<Sensor*> sensors;
 
 public:
-    void addSensor(Sensor* sensor) {
+    void addSensor(Sensor* sensor) 
+    {
         sensors.push_back(sensor);
     }
 
-    void monitorAndAdjust() {
-        for (Sensor* sensor : sensors) {
+    void monitorAndAdjust() 
+    {
+        for (Sensor* sensor : sensors) 
+        {
             sensor->gatherData();
             sensor->processData();
-            std::cout << "Adjusting controls based on sensor data." << std::endl;
+            std::cout << "Adjusting controls based on sensor data.\n" << std::endl;
         }
     }
 
-    ~AerospaceControlSystem()
+    ~AerospaceControlSystem()   // destructor
     {
         for (Sensor* sensor : sensors)
         {
@@ -98,15 +119,17 @@ public:
     }
 };
 
-int main() {
+// main function (question 1.5)
+int main() 
+{
     AerospaceControlSystem ctrlSys;
 
-    // Add sensors to the control system
+    // instantiating the objects from the aero class and invoking the addSensor function
     ctrlSys.addSensor(SensorFactory::createSensor("Temperature"));
-    ctrlSys.addSensor(SensorFactory::createSensor("Pressure"));
-    ctrlSys.addSensor(SensorFactory::createSensor("Position"));
+    ctrlSys.addSensor(SensorFactory::createSensor("Heading"));
+    ctrlSys.addSensor(SensorFactory::createSensor("Attitude"));
 
-    // Monitor and adjust the control system based on sensor data
+    // Invoking the monitor function
     ctrlSys.monitorAndAdjust();
 
     return 0;
