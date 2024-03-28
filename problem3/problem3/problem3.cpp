6fb7039752972b0c1f990fc1182b4b8670ed981f
Logan Feeney <lmf5732@psu.edu>
2024-03-27 22:21:50 -0400

    trying again

--- problem3/problem3/problem3.cpp
+++ problem3/problem3/problem3.cpp
@@ -29,3 +29,30 @@ void redirect_to_other_airport(int aircraft_number)
 {
     std::cout << "The approach pattern is full. Aircraft " << aircraft_number << " is redirected to another airport." << std::endl;
 }
+
+int main()
+{
+    std::vector<int>aircraft_in_queue = { 1,2,4,6,8,9,7,0,3,5 };  //update order of aircraft
+
+    for (int aircraft_number : aircraft_in_queue)
+    {
+        if (aircraft_number == 9 || aircraft_number == 7 || aircraft_number == 0 || aircraft_number == 3 || aircraft_number == 5)
+        {
+            request_landing(aircraft_number);
+        }
+        else
+        {
+            redirect_to_other_airport(aircraft_number);
+            cleared_landing(aircraft_number);
+            runway_clear();
+
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+    }
+
+    int total_duration = aircraft_in_queue.size();
+
+    std::cout << "Total duration: " << total_duration - 5 << " seconds" << std::endl;
+
+    return 0;
+}
