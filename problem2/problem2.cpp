41aa88a765efc304fe4d0fab0b4be37d56b1ed20
Logan Feeney <lmf5732@psu.edu>
2024-03-28 13:59:21 -0400

    almost done with 2

--- problem2/problem2.cpp
+++ problem2/problem2.cpp
@@ -7,78 +7,99 @@ Probem 2
 
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <chrono>
-#include <ctime>
 
 using namespace std;
 
-struct Tool {
-    bool taken;
+// Constants for the number of robots, tools, and times
+const int NUM_ROBOTS = 5;
+const int NUM_TOOLS = 5;
+const int TOOL_REACH_TIME = 1;
+const int TASK_TIME = 5;
+
+// Struct to represent a robot
+struct Robot 
+{
+    int id; // Robot ID
+    int taskRemaining{ 1 }; // initial task for each robot
+    bool hasTools; // check if the robot has tools
+    int tasks_left{ 1 };
 };
 
-struct Robot {
-    int position;
-    bool hasTool1;
-    bool hasTool2;
-    bool completedTask;
-};
+// Function to simulate the robot tasks
+void simulateRobotTasks(vector<Robot>& robots) 
+{
+    int totalTime = 0; // Initialize total time
+    bool allTasksCompleted = false; // check if all tasks are completed
 
-// Function to check if a robot can start a task
-bool canStartTask(const Robot& robot, const vector<Tool>& tools) {
-    return robot.hasTool1 && robot.hasTool2 && !tools[robot.position].taken;
+    // loop will run until all tasks are completed
+    while (!allTasksCompleted)
+    {
+        allTasksCompleted = true; // at the beginning of each while loop, set check to true and if a robot has not finished task, it will switch back to false
+        
+        // Loop through each robot
+        for (int i = 0; i < NUM_ROBOTS; i++) 
+        {
+            // Check if the robot still has tasks to complete
+            if (robots[i].tasks_left > 0) 
+            {
+                allTasksCompleted = false;  // Set the flag to false if any task is remaining
+
+                // Check if the robot has tools
+                if (robots[i].hasTools) 
+                {
+                    cout << "Robot " << robots[i].id << " is completing its task." << endl;
+                    robots[i].tasks_left -= 1; // Decrease the remaining task time
+                    totalTime += 1; // Increase the total time taken
+
+                    // Check if the task is completed
+                    if (robots[i].tasks_left == 0) 
+                    {
+                        robots[i].hasTools = false; // Robot no longer has tools
+                        cout << "Robot " << robots[i].id << " has completed its task." << endl;
+                    }
+                }
+                else 
+                {
+                    cout << "Robot " << robots[i].id << " is waiting for tools." << endl;
+                    totalTime += TOOL_REACH_TIME; // Time to reach for tools
+                    robots[i].hasTools = true; // Robot now has tools
+                }
+            }
+        }
+
+        // If not all tasks are completed, add time for the next round
+        if (!allTasksCompleted) 
+        {
+            totalTime += TASK_TIME;
+
+            // Adjust for pairs working together
+            for (int i = 0; i < NUM_ROBOTS; i++) 
+            {
+                if (robots[i].hasTools && robots[(i + 2) % NUM_ROBOTS].hasTools) 
+                {
+                    totalTime -= TOOL_REACH_TIME; // Reduce time as both robots have tools
+                    break;
+                }
+            }
+        }
+    }
+
+    // Print the total time taken
+    cout << "Total time taken: " << totalTime << " seconds." << endl;
 }
 
 int main() {
-    const int numRobots = 5;
-    const int numTools = 5;
-    const int moveTime = 1;
-    const int taskTime = 5;
-
-    vector<Robot> robots(numRobots);
-    vector<Tool> tools(numTools, { false }); // Initially, all tools are available
-
-    // Initialize robots
-    for (int i = 0; i < numRobots; ++i) {
-        robots[i].position = i;
-        robots[i].hasTool1 = false;
-        robots[i].hasTool2 = false;
-        robots[i].completedTask = false;
+    // Create vector of robots and initialize their properties
+    vector<Robot> robots(NUM_ROBOTS);
+    for (int i = 0; i < NUM_ROBOTS; i++) 
+    {
+        robots[i].id = i + 1; // Assign robot ID
+        robots[i].taskRemaining = TASK_TIME; // Set initial task time remaining
+        robots[i].hasTools = false; // Initially, robots do not have tools
     }
 
-    int totalTime = 0;
+    // Call the function to simulate robot tasks
+    simulateRobotTasks(robots);
 
-    // Simulate until all tasks are completed
-    while (any_of(robots.begin(), robots.end(), [](const Robot& r) { return !r.completedTask; })) {
-        // Move robots
-        for (int i = 0; i < numRobots; ++i) {
-            int nextPos = (robots[i].position + 1) % numRobots;
-            robots[i].position = nextPos;
-        }
-
-        // Check if robots can grab tools and start tasks
-        for (int i = 0; i < numRobots; ++i) {
-            if (!robots[i].hasTool1 && !robots[i].hasTool2) {
-                robots[i].hasTool1 = true;
-                robots[i].hasTool2 = true;
-                tools[robots[i].position].taken = true; // Robot takes both tools
-                auto now = chrono::system_clock::to_time_t(chrono::system_clock::now());
-                cout << "Robot " << i << " picks up tools at position " << robots[i].position << " at " << ctime(&now);
-            }
-
-            // Check if the robot can start a task
-            if (canStartTask(robots[i], tools) && !robots[i].completedTask) {
-                robots[i].completedTask = true;
-                totalTime += taskTime; // Add task completion time
-                auto now = chrono::system_clock::to_time_t(chrono::system_clock::now());
-                cout << "Robot " << i << " starts task at position " << robots[i].position << " at " << ctime(&now);
-            }
-        }
-
-        totalTime += moveTime; // Add time for robot movement
-    }
-
-    cout << "Total time taken: " << totalTime << " seconds\n";
-
-    return 0;
+    return 0; // Exit program
 }
